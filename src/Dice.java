2a11512568ad519c8b013939c8009b683de8b2f4
AnthonySleeps <anthony.amedome25@bishopireton.org>
2023-09-08 13:16:37 -0400

    Merge branch 'main' of https://github.com/AnthonySleeps/Monopoly-Game.git into main

--- src/Dice.java
+++ src/Dice.java
@@ -1,20 +1,24 @@
-import java.util.ArrayList;
 import java.util.Random;
-
-public class Dice extends ArrayList<Integer>{
-    private int d1, d2, d3, d4, d5, d6;
-
-    public Dice(int n1, int n2, int n3, int n4, int n5, int n6) {
-        d1 = n1;
-        d2 = n2;
-        d3 = n3;
-        d4 = n4;
-        d5 = n5;
-        d6 = n6;
-    }
-
-    public int roll(){
-        return 3;
-    }
-    
-}
+public class Dice{
+	private int d1, d2, d3, d4, d5, d6;
+	Random rand = new Random();
+	public Dice(int n1, int n2, int n3, int n4, int n5, int n6) {
+		d1 = n1;
+		d2 = n2;
+		d3 = n3;
+		d4 = n4;
+		d5 = n5;
+		d6 = n6;
+		}
+	public int roll(){
+		int rollInt = (rand.nextInt(6))+1;
+		switch (rollInt) {
+		case 1:return d1;
+		case 2:return d2;
+		case 3:return d3;
+		case 4:return d4;
+		case 5:return d5;
+		case 6:return d6;
+		}
+		return 1;}
+	}
